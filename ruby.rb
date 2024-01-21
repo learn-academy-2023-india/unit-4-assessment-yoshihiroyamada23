66d214958f4979ab7c3a27caee334112267de11d
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-01-21 14:37:50 -0800

    completed assessment

--- ruby.rb
+++ ruby.rb
@@ -7,4 +7,23 @@
 # Add comments to your code explaining your approach
 
 class Bike
+    attr_accessor :model, :current_speed, :wheels
+    def initialize (model)
+    @model = model
+    @current_speed = 0
+    @wheels = 2
+    end
+
+    def pedal_faster speed
+        @current_speed += speed
+    end
+    
+    def brake speed
+       @current_speed -= speed
+       @current_speed = @current_speed >= 0 ? @current_speed: 0
+    end
+
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{current_speed} mph."
+    end
 end
